d1285dddf84caa2b7ea75f23652386ac2c8a6dce
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-07 23:22:56 +0530

    init

---
+++ 7-1-25/Input.java
@@ -0,0 +1,9 @@
+class Input {
+    public static void main(String args[])
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = a + b;
+        System.out.println("Sum of "+a+" and "+b+" is " + c);
+    }
+}
