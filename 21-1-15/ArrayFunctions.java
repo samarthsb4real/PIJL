0a5069920d49a7154e63ecf7ddc7cff4ff90dd19
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-21 16:03:36 +0530

    add OddEven method to ArrayFunctions for separating odd and even integers

--- 21-1-15/ArrayFunctions.java
+++ 21-1-15/ArrayFunctions.java
@@ -2,9 +2,35 @@
 import java.util.*;
 
 class ArrayFunctions {
-	void display(int[] array) {
-		UserInput in = new UserInput();
-		System.out.println("Array is: " + Arrays.toString(array));
+
+	UserInput in = new UserInput();
+
+	void display(ArrayList<Integer> array) {
+		System.out.println("Array is: " + array);
 	}
 
+	void OddEven(int[] array)
+	{
+		// int[] even = new int[array.length];
+		// int[] odd = new int[array.length];
+		// int ODD_INDEX = 0;
+		// int EVEN_INDEX = 0;
+
+		ArrayList<Integer> even = new ArrayList<Integer>();
+		ArrayList<Integer> odd = new ArrayList<Integer>();
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] % 2 == 0) {
+				// even[EVEN_INDEX] = array[i];
+				// EVEN_INDEX++;
+				even.add(array[i]);
+			} else {
+				// odd[ODD_INDEX] = array[i];
+				// ODD_INDEX++;
+				odd.add(array[i]);
+			}
+		}
+		display(even);
+		display(odd);
+	}
 }
--- 21-1-15/Main.java
+++ 21-1-15/Main.java
@@ -2,6 +2,6 @@ class Main{
     public static void main(String[] args){
         UserInput in = new UserInput();
         ArrayFunctions functions = new ArrayFunctions();
-        functions.display(in.arrayInput());
+        functions.OddEven(in.arrayInput());
     }
 }
