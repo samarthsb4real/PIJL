75dd91c01f47dbd9ec0f8764ac8301e1313a97e1
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-21 15:19:01 +0530

    remove Main class and add ArrayFunctions and UserInput classes for array input handling

---
+++ 21-1-15/ArrayFunctions.java
@@ -0,0 +1,10 @@
+//ArrayFunctions.java
+import java.util.*;
+
+class ArrayFunctions {
+	void display(int[] array) {
+		UserInput in = new UserInput();
+		System.out.println("Array is: " + Arrays.toString(in.arrayInput()));
+	}
+
+}
