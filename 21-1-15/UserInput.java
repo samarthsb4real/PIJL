afcc960b18c7da298c83a7a523f6af56fc7f9489
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-21 14:54:10 +0530

    refactor UserInput class and relocate to new directory structure

--- 21-1-15/UserInput.java
+++ 21-1-15/UserInput.java
@@ -5,7 +5,7 @@ class UserInput {
         int[] arr = new int[5];
         System.out.println("Enter 5 numbers: ");
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         sc.close();
