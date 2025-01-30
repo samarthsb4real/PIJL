105aac8924cdd0a7f8e413092ad7a9ca4c2ebb31
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-30 23:20:06 +0530

    fix: update Main class to allow input for multiple students

--- Assignment-3/Main.java
+++ Assignment-3/Main.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);    
-        System.out.println("Enter Name: ");
-        String name = sc.nextLine();
-        System.out.println("Enter PRN: ");
-        long prn = sc.nextLong();
-        System.out.println("Enter CGPA: ");
-        double cgpa = sc.nextDouble();
-        System.out.println("Enter Branch: ");
-        String branch = sc.next();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of students: ");
+        int n = sc.nextInt();
 
-        StudentOperations operations = new StudentOperations();
-        Student student = new Student(name, prn, cgpa, branch);
-        operations.addStudents(student);
-        operations.displayStudents();
+        for (int i = 0; i <= n; i++) {
+            System.out.println("Enter Name: ");
+            String name = sc.nextLine();
+            System.out.println("Enter PRN: ");
+            long prn = sc.nextLong();
+            System.out.println("Enter CGPA: ");
+            double cgpa = sc.nextDouble();
+            System.out.println("Enter Branch: ");
+            String branch = sc.next();
 
-        sc.close();
+            StudentOperations operations = new StudentOperations();
+            Student student = new Student(name, prn, cgpa, branch);
+            operations.addStudents(student);
+            operations.displayStudents();
+
+            sc.close();
+        }
     }
 }
