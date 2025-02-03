cd01861b1420a7c1aa3df011e7a8391d4b0dfc0c
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-03 15:37:40 +0530

    feat: add functionality to input numbers in Main class

--- Assignment-2/Main.java
+++ Assignment-2/Main.java
@@ -13,6 +13,11 @@ public class Main {
         int n = scanner.nextInt();
         int[] numbers = new int[n];
 
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
         scanner.close();
     }
 }
