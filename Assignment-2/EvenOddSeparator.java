52b6ae8a146372305fdb150bfed67d9bbd709e5f
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-03 09:44:16 +0530

    feat: add EvenOddSeparator class to separate even and odd numbers

---
+++ Assignment-2/EvenOddSeparator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class EvenOddSeparator {
+    // Method to separate even and odd numbers
+    public void separate(int[] numbers) {
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+    }
+}
