e151c3f8b1a267c4399cfc2eaa91c5a5e256da91
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-03 15:37:05 +0530

    feat: add Main class with basic structure and scanner initialization

---
+++ Assignment-2/Main.java
@@ -0,0 +1,12 @@
+// Name: Samarth Sandesh Bhadane
+// PRN: 24070126503
+// Batch: A2
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+    }
+}
