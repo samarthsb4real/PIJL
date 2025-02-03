63480ac805577ce4cc4e2362c45a8b941b12ff76
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-03 09:47:05 +0530

    feat: add SmallestDistanceFinder class to find the smallest distance between neighboring elements

---
+++ Assignment-2/SmallestDistanceFinder.java
@@ -0,0 +1,11 @@
+public class SmallestDistanceFinder {
+    // Method to find the smallest distance between neighboring elements
+    public int findSmallestDistance(int[] numbers) {
+        if (numbers.length < 2) return -1;
+
+        int minDistance = Math.abs(numbers[1] - numbers[0]);
+        int index = 0;
+
+        return index;
+    }
+}
