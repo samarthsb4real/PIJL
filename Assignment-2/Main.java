13be45ae777d027cf098a13029f301742ba81385
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-03 15:37:22 +0530

    feat: add user input functionality for number of elements in Main class

--- Assignment-2/Main.java
+++ Assignment-2/Main.java
@@ -7,6 +7,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Taking user input for numbers using for loop
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+
         scanner.close();
     }
 }
