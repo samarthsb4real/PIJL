55dfa8cc402b5411e1eab9bbb64dde891cba3f66
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-10 15:00:56 +0530

    feat: add abstract Shape class with constructors and abstract methods for area and perimeter

---
+++ Shape/Shape.java
@@ -0,0 +1,28 @@
+//Shape.java
+
+abstract class Shape {
+    protected double dim_one, dim_two, dim_three;
+    int num_sides;
+
+    // Zero parameter constructor
+    public Shape() {
+        dim_one = dim_two = dim_three = 0;
+        num_sides = 0;
+    }
+
+    // Parameterized constructor
+    public Shape(double dim_one, double dim_two, double dim_three, int num_sides) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.num_sides = num_sides;
+    }
+
+    abstract double area(); // Abstract method to calculate area
+    abstract double perimeter(); // Abstract method to calculate perimeter
+    // Abstract method to calculate area
+    public int getNumSides() {
+        return num_sides;
+    }
+
+}
