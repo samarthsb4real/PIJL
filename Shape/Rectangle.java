7dc9d95e05fbdaab61eab1343874e19b9c34b85b
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-10 15:10:36 +0530

    setting up functions for rectangle class

--- Shape/Rectangle.java
+++ Shape/Rectangle.java
@@ -4,4 +4,12 @@ class Rectangle extends Shape {
     public Rectangle(double dim_one, double dim_two) {
         super(dim_one, dim_two, 0, 4);
     }
+
+    public double area() {
+        return dim_one * dim_two;
+    }
+
+    public double perimeter() {
+        return 2 * (dim_one + dim_two);
+    }
 }
