f5551bbf39bcd9ae51c5caf16b557917f0702451
samarthsb4real <samarthbhadane23@gmail.com>
2025-02-11 14:32:01 +0530

    feat: add display method and quack behavior management to Duck class

--- 11-2-25/src/Duck.java
+++ 11-2-25/src/Duck.java
@@ -1,3 +1,14 @@
 public abstract class Duck {
     QuackBehavior quackBehavior;
+
+    abstract public void display();
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        this.quackBehavior = qb;
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
 }
